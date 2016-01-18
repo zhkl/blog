699ab74a2c25956591971db942ae88d60e093873
kaalzh <kaalzh123@gmail.com>
2016-01-18 17:39:21 +0800

    fix

---
+++ config/puma.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env puma
+
+directory '/home/deploy/apps/blog/current'
+rackup "/home/deploy/apps/blog/current/config.ru"
+environment 'staging'
+
+pidfile "/home/deploy/apps/blog/shared/tmp/pids/puma.pid"
+state_path "/home/deploy/apps/blog/shared/tmp/pids/puma.state"
+stdout_redirect '/home/deploy/apps/blog/shared/log/puma_access.log', '/home/deploy/apps/blog/shared/log/puma_error.log', true
+
+
+threads 0,16
+
+bind 'unix:///home/deploy/apps/blog/shared/tmp/sockets/puma.sock'
+
+workers 0
+
+
+
+prune_bundler
+
+
+on_restart do
+  puts 'Refreshing Gemfile'
+  ENV["BUNDLE_GEMFILE"] = "/home/deploy/apps/blog/current/Gemfile"
+end
+
+
