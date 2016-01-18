153acda0bd6bca91c48f17b1f0bf97d1517144f3
kaalzh <kaalzh123@gmail.com>
2016-01-18 17:14:48 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,13 @@ set :application,     'blog'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+set :deploy_to, '/home/deploy/apps/blog'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :staging
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
