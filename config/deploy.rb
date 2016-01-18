99ed093593a30693d298dc2d3c8f5b6ee137f43d
kaalzh <kaalzh123@gmail.com>
2016-01-18 16:37:14 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,20 +11,6 @@ set :user, 'deploy'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/apps/blog'
 
-
-# Don't change these unless you know what you're doing
-set :stage,           :staging
-set :deploy_via,      :remote_cache
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-
 # Default value for :scm is :git
 # set :scm, :git
 
