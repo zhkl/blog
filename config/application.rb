66a328e43d82728b9c9d51e360e5f54155d5e1c9
kaalzh <kaalzh123@gmail.com>
2016-01-18 12:20:33 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Blog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.eager_load = false
   end
 end
