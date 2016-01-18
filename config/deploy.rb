dfc38652c6f05d8a837ac72ab96d42d3e98a90fd
kaalzh <kaalzh123@gmail.com>
2016-01-18 10:49:05 +0800

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :user, 'deploy'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/apps/blog'
 
 # Default value for :scm is :git
 # set :scm, :git
