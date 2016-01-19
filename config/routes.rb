b34cd2a65115a0a610ffcaf7702f5cdbde52f390
kaalzh <kaalzh123@gmail.com>
2016-01-19 17:08:00 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages/welcome', controller: 'pages/welcome'
+  root to: 'pages#welcome'
   get  'pages/welcome', controller: 'pages/welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
