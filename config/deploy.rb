04f6502d49f427b9aa064c441963d4f8ec8ebadf
kaalzh <kaalzh123@gmail.com>
2016-01-19 15:46:58 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,7 @@ set :use_sudo,        false
 set :stage,           :staging
 set :deploy_via,      :remote_cache
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/blog-puma.sock"
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
