140512dca3179a97f9989c07267adc9cdd3d6f0b
kaalzh <kaalzh123@gmail.com>
2016-01-19 15:20:50 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:zhkl/blog.git'
 set :user, 'deploy'
 
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    1
 
 set :deploy_to, '/home/deploy/apps/blog'
 
