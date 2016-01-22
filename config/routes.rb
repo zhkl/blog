9d36075d6efb2534c6483584c3ef6a90d8747c85
kaalzh <kaalzh123@gmail.com>
2016-01-22 12:42:40 +0800

    add post

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#welcome'
   get  'pages/welcome', controller: 'pages/welcome'
 
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
